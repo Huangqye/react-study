b8adcf544b47219b2aa2a0a4a4bbe9e60eee9fe0
hqy <469346971@qq.com>
2024-02-18 10:45:28 +0800

    初始化

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
-import { useEffect, useState } from "react";
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-  // 闭包陷阱
-  // 这里出现异步
-  useEffect(() => {
-    setInterval(() => {
-      console.log(count);
-      setCount(count + 1);
-      // 解决方法一
-    }, 1000);
-    // 问题在这里 []，执行并保留第一次的function
-  }, []);
-
-  return <div>{count}</div>;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
