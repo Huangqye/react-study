6623bdbfee8664052b51b82b7cfcb943149a0f19
hqy <469346971@qq.com>
2024-06-28 10:09:05 +0800

    icon组件2

--- icon-component/src/App.tsx
+++ icon-component/src/App.tsx
@@ -1,10 +1,16 @@
 import "./App.css";
+import { createFromIconFont } from "./Icon/createFrontIconfont";
 import { IconAdd } from "./Icon/icons/IconAdd";
+
+const IconFont = createFromIconFont(
+  "//at.alicdn.com/t/c/font_4066996_d052yem1to8.js"
+);
 
 function App() {
   return (
     <div style={{ padding: "50px" }}>
       <IconAdd size="40px" spin style={{ color: "blue" }}></IconAdd>
+      <IconFont type="icon-zhongdian" size="40px" fill="blue"></IconFont>
     </div>
   );
 }
