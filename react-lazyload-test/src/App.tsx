29deba28210fd51e509b26f9c7038d4abe1c5167
hqy <469346971@qq.com>
2024-06-28 13:21:54 +0800

    reactLazyLoad

---
+++ react-lazyload-test/src/App.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import img1 from "./WechatIMG33.png";
+import img2 from "./WechatIMG33.png";
+import LazyLoad from "react-lazyload";
+
+const LazyHuang = React.lazy(() => import("./Huang"));
+export default function App() {
+  return (
+    <div>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <LazyLoad placeholder={<div>loading...</div>}>
+        {/* <img src={img1} /> */}
+        <LazyHuang />
+      </LazyLoad>
+      <LazyLoad placeholder={<div>loading...</div>}>
+        <img src={img2} />
+      </LazyLoad>
+    </div>
+  );
+}
---
+++ react-lazyload-test/src/Huang.tsx
@@ -0,0 +1,3 @@
+export default function Huang() {
+  return "出现";
+}
