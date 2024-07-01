6ebba68cecd4351e1532bbb4dacb4005b45d0c96
hqy <469346971@qq.com>
2024-07-01 15:22:08 +0800

    space组件

--- space-component/src/App.tsx
+++ space-component/src/App.tsx
@@ -1,19 +1,38 @@
 import "./App.css";
 import Space from "./Space";
+import { ConfigProvider } from "./Space/ConfigProvider";
 
 function App() {
   return (
-    <div>
-      <Space>
-        <div className="box"></div>
-        <div className="box"></div>
-        <div className="box"></div>
-      </Space>
-    </div>
+    // <Space
+    //   direction="horizontal"
+    //   align="end"
+    //   className="container"
+    //   wrap
+    //   size={["large", "small"]}
+    // >
+    //   <div className="box"></div>
+    //   <div className="box"></div>
+    //   <div className="box"></div>
+    // </Space>
     // <Test>
     //   {[[<div>111</div>, <div>222</div>], [<div>333</div>]]}
     //   <span>hello world</span>
     // </Test>
+    <div>
+      <ConfigProvider space={{ size: 20 }}>
+        <Space direction="horizontal">
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+        </Space>
+        <Space direction="vertical">
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+        </Space>
+      </ConfigProvider>
+    </div>
   );
 }
 
--- space-component/src/Space/index.tsx
+++ space-component/src/Space/index.tsx
@@ -1,5 +1,7 @@
 import classNames from "classnames";
 import React from "react";
+import "./index.scss";
+import { ConfigContext } from "./ConfigProvider";
 
 export type SizeType = "small" | "middle" | "large" | number | undefined;
 
@@ -15,11 +17,12 @@ export interface SpaceProps extends React.HTMLAttributes<HTMLDivElement> {
 }
 
 const Space: React.FC<SpaceProps> = (props) => {
+  const { space } = React.useContext(ConfigContext);
   const {
     className,
     style,
     children,
-    size = "small",
+    size = space?.size || "small",
     direction = "horizontal",
     align,
     split,
@@ -27,14 +30,41 @@ const Space: React.FC<SpaceProps> = (props) => {
     ...otherProps
   } = props;
 
+  const spaceSize = {
+    small: 8,
+    middle: 16,
+    large: 24,
+  };
+
+  function getNumberSize(size: SizeType) {
+    return typeof size === "string" ? spaceSize[size] : size || 0;
+  }
+
   const childNodes = React.Children.toArray(props.children);
+
+  const otherStyles: React.CSSProperties = {};
+
+  const [horizontalSize, verticalSize] = React.useMemo(
+    () =>
+      ((Array.isArray(size) ? size : [size, size]) as [SizeType, SizeType]).map(
+        (item) => getNumberSize(item)
+      ),
+    [size]
+  );
+
+  otherStyles.columnGap = horizontalSize;
+  otherStyles.rowGap = verticalSize;
+
+  if (wrap) {
+    otherStyles.flexWrap = "wrap";
+  }
 
   const mergedAlign =
     direction === "horizontal" && align === undefined ? "center" : align;
 
   const cn = classNames(
     "space",
-    "space-${direction}",
+    `space-${direction}`,
     {
       [`space-align-${mergedAlign}`]: mergedAlign,
     },
@@ -44,14 +74,21 @@ const Space: React.FC<SpaceProps> = (props) => {
   const nodes = childNodes.map((child: any, i) => {
     const key = (child && child.key) || `space-item-${i}`;
     return (
-      <div className="space-item" key={key}>
-        {child}
-      </div>
+      <>
+        <div className="space-item" key={key}>
+          {child}
+        </div>
+        {i < childNodes.length && split && (
+          <span className={`${className}-split`} style={style}>
+            {split}
+          </span>
+        )}
+      </>
     );
   });
 
   return (
-    <div className={cn} style={style} {...otherProps}>
+    <div className={cn} style={{ ...style, ...otherStyles }} {...otherProps}>
       {nodes}
     </div>
   );
